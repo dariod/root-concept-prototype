3e524d8d15e198c130442beadf4b5bb4a544fcb4
Dario Dorella <dario.dorella@gmail.com>
2017-07-29 18:06:50 -0700

    Extended the virtualRoot to be a list of directories

--- ls.py
+++ ls.py
@@ -10,8 +10,11 @@ def readConfigFile(path=None):
         path = './ls.cfg'
     try:
         fdCfg=open(path,'ru')
-        newRoot=fdCfg.read().strip()
+        newRoot=fdCfg.readlines()
         fdCfg.close()
+        # Strip the wnelines from the entries
+        for i in range(len(newRoot)):
+            newRoot[i]=newRoot[i].strip()
         return(newRoot)
     except Exception as err:
         sys.stderr.write(str(err) + '\n')
@@ -29,21 +32,27 @@ if not rootDir:
 # the first argument after the exctuable name.
 # If no argument, list the root directroy contents
 if len(sys.argv) < 2:
-    sys.stdout.write(rootDir + '\n')
+    for i in sorted(rootDir):
+        sys.stdout.write(i + '\n')
 else:
     # The directroy requested is / then we still just print rootDir
     if sys.argv[1] == '/':
-        sys.stdout.write(rootDir + '\n')
+        for i in sorted(rootDir):
+            sys.stdout.write(i + '\n')
     else:
         # We really got a full path
         # Does it start with rootDir?
-        if sys.argv[1].startswith(rootDir):
-            dirToList=sys.argv[1]
-        # Is it a path realtive to a rootDir?
-        elif os.path.join('/',sys.argv[1]).startswith(rootDir):
-            dirToList=os.path.join('/',sys.argv[1])
-        # If none of the above, we cannot list
-        else:
+        dirToList=None
+        for i in rootDir:
+            if sys.argv[1].startswith(i):
+                dirToList=sys.argv[1]
+                break;
+            # Is it a path realtive to a rootDir?
+            elif os.path.join('/',sys.argv[1]).startswith(i):
+                dirToList=os.path.join('/',sys.argv[1])
+                break;
+            # If none of the above, we cannot list
+        if not dirToList:
             sys.stderr.write('Cannot list ' + sys.argv[1] + '\n')
             sys.exit(1)
 
